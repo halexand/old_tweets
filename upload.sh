933517681b5eb0e05891fe24daaafdeadcc30cd5
Wolfgang Huber <wolfgang.huber@embl.org>
2024-11-24 11:37:08 +0100

    Not needed

--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-#  This will upload the report to https://www.huber.embl.de/users/whuber/pca-animation/
-#
-chmod 644 index.html
-find index_files -type d -exec chmod 755 {} \;
-find index_files -type f -exec chmod 644 {} \;
-rsync -rvlpt index.html index_files wh.css pca-animation.mp4 datatransfer.embl.de:/g/huber/www-huber/users/whuber/pca-animation/
-
